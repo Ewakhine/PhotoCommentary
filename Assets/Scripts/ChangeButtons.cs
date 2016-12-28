015ad1e04f1d6ef6d53917314779f3a865dd21a8
Ewakhine <Ewakhine>
2016-12-28 23:50:12 +0100

    First commit. Create a main scene with for elements : a rawImage, two comment objects and one navigationObject. Create a behaviour script for comments objects.

---
+++ Assets/Scripts/ChangeButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeButtons : MonoBehaviour {
+
+    GameObject largePanel;
+    GameObject miniPanel;
+
+    Button nextButton;
+    Button previousButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Init()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Comment.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Place this script on a GameObject with those named children :
+// - Comment Panel
+// -- PinClose Comment Button
+// - Mini Panel
+public class Comment : MonoBehaviour {
+
+    GameObject largePanel;
+    GameObject miniPanel;
+    Button pinCloseButton;
+
+    bool _isInitCompleted = false;
+
+    bool _isMiniPanelShowed;
+    bool _isLargePanelShowed;
+    bool _isCommentPined;
+
+///////////////////////////////////////////////////////////////
+/// GENERAL FUNCTIONS /////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+    void Start () {
+        Init();
+
+        if(_isInitCompleted){
+            ShowLargePanel(false);
+            ShowMiniPanel();
+            Btn_PinComment();
+        }
+    }
+    /*********************************************************/
+
+    void Update () {
+        if(_isInitCompleted)
+            CheckPanels();
+    }
+    /*********************************************************/
+
+///////////////////////////////////////////////////////////////
+/// PRIVATE FUNCTIONS /////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+    void Init() {
+        largePanel = transform.Find("Comment Panel").gameObject;
+        miniPanel = transform.Find("Mini Panel").gameObject;
+        pinCloseButton = largePanel.transform.Find("PinClose Comment Button").GetComponent<Button>();
+
+        Debug.Log(this.name + " : Init is complete");
+        _isInitCompleted = true;
+    }
+    /*********************************************************/
+
+    void CheckPanels() {
+        bool mouseOnMiniPanel = RectTransformUtility.RectangleContainsScreenPoint(miniPanel.GetComponent<RectTransform>(), Input.mousePosition);
+        bool mouseOnLargePanel = RectTransformUtility.RectangleContainsScreenPoint(largePanel.GetComponent<RectTransform>(), Input.mousePosition);
+
+        if (!_isLargePanelShowed && mouseOnMiniPanel) {
+            // Debug.Log("OPEN LARGE");
+            ShowLargePanel();
+            ShowMiniPanel(false);
+        }
+        else if (!_isMiniPanelShowed && !mouseOnLargePanel && !_isCommentPined) {
+            // Debug.Log("QUIT LARGE");
+            ShowLargePanel(false);
+            ShowMiniPanel();
+        }
+    }
+    /*********************************************************/
+
+    void ShowLargePanel(bool a_showLargePanel = true) {
+        _isLargePanelShowed = a_showLargePanel;
+        largePanel.GetComponent<CanvasGroup>().alpha = a_showLargePanel ? 1 : 0;
+        largePanel.GetComponent<CanvasGroup>().blocksRaycasts = a_showLargePanel;
+    }
+    /*********************************************************/
+
+    void ShowMiniPanel(bool a_showMiniPanel = true) {
+        _isMiniPanelShowed = a_showMiniPanel;
+        miniPanel.GetComponent<CanvasGroup>().alpha = a_showMiniPanel ? 1 : 0;
+        miniPanel.GetComponent<CanvasGroup>().blocksRaycasts = a_showMiniPanel;
+
+    }
+    /*********************************************************/
+
+    void Btn_PinComment() {
+        pinCloseButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        pinCloseButton.GetComponent<Button>().onClick.AddListener(() => Btn_CloseComment());
+        pinCloseButton.transform.GetChild(0).GetComponent<Text>().text = "PIN";
+        _isCommentPined = false;
+    }
+    /*********************************************************/
+
+    void Btn_CloseComment() {
+        pinCloseButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        pinCloseButton.GetComponent<Button>().onClick.AddListener(() => Btn_PinComment());
+        pinCloseButton.transform.GetChild(0).GetComponent<Text>().text = "X";
+        _isCommentPined = true;
+    }
+    /*********************************************************/
+}
