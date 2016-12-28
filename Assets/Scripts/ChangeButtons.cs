300b947f169ca010eeda2038a329d84d079af65b
Ewakhine <Ewakhine>
2016-12-29 00:49:57 +0100

    Add management of images loaded in Resources/Photos.

--- Assets/Scripts/ChangeButtons.cs
+++ Assets/Scripts/ChangeButtons.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 // Place this script on a GameObject with those named children :
 // - Large Panel
@@ -7,8 +8,13 @@ using UnityEngine.UI;
 // - Mini Panel
 public class ChangeButtons : TwoSizesPanel {
 
+    public RawImage currentPhoto;
+    int _currentIndex;
+
     Button nextButton;
     Button previousButton;
+
+    Texture[] photos;
 
 ///////////////////////////////////////////////////////////////
 /// GENERAL FUNCTIONS /////////////////////////////////////////
@@ -37,8 +43,31 @@ public class ChangeButtons : TwoSizesPanel {
         nextButton = largePanel.transform.Find("Next Button").GetComponent<Button>();
         previousButton = largePanel.transform.Find("Previous Button").GetComponent<Button>();
 
+        photos = Resources.LoadAll<Texture>("");
+        /*Debug.Log(photos.Length);
+        for (int i = 0; i < photos.Length; i++)
+            Debug.Log(photos[i].name);*/
+        if (photos.Length > 0)
+        {
+            _currentIndex = 0;
+            currentPhoto.texture = photos[_currentIndex];
+        }
+
         Debug.Log(this.name + " : Init is completed.");
         _isInitCompleted = true;
+    }
+    /*********************************************************/
+
+    public void Btn_NextImage() {
+        if (_currentIndex < photos.Length -1)
+            _currentIndex++;
+        currentPhoto.texture = photos[_currentIndex];
+    }
+    /*********************************************************/
+    public void Btn_PreviousImage() {
+        if (_currentIndex > 0 )
+            _currentIndex--;
+        currentPhoto.texture = photos[_currentIndex];
     }
     /*********************************************************/
 }
